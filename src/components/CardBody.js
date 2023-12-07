01ade1557e73728e3a6818eb4da7f02a846fed74
Dhruv <dhruvbhatnagar4@gmail.com>
2023-12-07 15:19:52 +0530

    upto episode-5 part 3

--- src/components/CardBody.js
+++ src/components/CardBody.js
@@ -4,74 +4,22 @@ import { useState } from "react";
 
 const CardBody = () => {
     // 2) react variable
-    const [listOfRestaunt2, setlistOfRestaunt2] = useState(
-        {
-            data: {
-                id: "73011",
-                name: "KFC",
-                cloudinaryImageId: "bdcd233971b7c81bf77e1fa4471280eb",
-                cuisines: ["American", "Snacks", "Biryani"],
-
-                costForTwo: 30000,
-                deliveryTime: 31,
-                avgRating: "3.8",
-
-            }
-        },
-        {
-            data: {
-                id: "73012",
-                name: "Dominos",
-                cloudinaryImageId: "bdcd233971b7c81bf77e1fa4471280eb",
-                cuisines: ["American", "Snacks", "Biryani"],
-                costForTwo: 30000,
-                deliveryTime: 31,
-                avgRating: "4.5",
-            },
-        }
-    )
-
-    // 1) normal js func
-    let listOfRestaunt = [
-        {
-            data: {
-                id: "73011",
-                name: "KFC",
-                cloudinaryImageId: "bdcd233971b7c81bf77e1fa4471280eb",
-                cuisines: ["American", "Snacks", "Biryani"],
-
-                costForTwo: 30000,
-                deliveryTime: 31,
-                avgRating: "3.8",
-
-            }
-        },
-        {
-            data: {
-                id: "73012",
-                name: "Dominos",
-                cloudinaryImageId: "bdcd233971b7c81bf77e1fa4471280eb",
-                cuisines: ["American", "Snacks", "Biryani"],
-                costForTwo: 30000,
-                deliveryTime: 31,
-                avgRating: "4.5",
-            },
-        }
-    ]
+    const [newResList, setNewResList] = useState(resObj)
 
     return (
         <div className="card-body">
             <button className="filter-btn" onClick={() => {
-                listOfRestaunt = listOfRestaunt.filter((items) => {
-                    console.log("using filter")
+                const filteredRes = newResList.filter((items) => {
                     return items.data.avgRating > 4
                 });
-                console.log(listOfRestaunt);
-            }}>top Restaurant</button>
+                setNewResList(filteredRes);
+            }}>
+                top Restaurant
+            </button>
 
             <div className="res-container">
                 {
-                    listOfRestaunt.map((items) => {
+                    newResList.map((items) => {
                         return <RestaurantCard key={items.data.id} resdata={items} />
                     })
                 }
