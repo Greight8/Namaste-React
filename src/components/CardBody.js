afff05a217bccb6a087f10803a2aaa38cb63b7ee
Dhruv <dhruvbhatnagar4@gmail.com>
2023-12-08 23:47:04 +0530

    upto episode-6 part 4

--- src/components/CardBody.js
+++ src/components/CardBody.js
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
 
 const CardBody = () => {
+    console.log("cardBody rendered")
+
     // 1) react variable
     const [newResList, setNewResList] = useState([]);
 
@@ -10,12 +12,14 @@ const CardBody = () => {
         fetchData()
     }, [])
 
+    const [searchText, setSearchText] = useState("")
+
     const fetchData = async () => {
         let url = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
 
         let response = await fetch(url);
         let myData = await response.json();
-        console.log(myData);
+        // console.log(myData);
         setNewResList(myData.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
     }
 
@@ -24,6 +28,21 @@ const CardBody = () => {
     return newResList.length === 0 ? <Shimmer /> :
         (
             <div className="card-body">
+                <div className="search">
+                    <input className="search-box" type="text" onChange={(e) => {
+                        // console.log(e);
+                        setSearchText(e.target.value);
+                    }} value={searchText} />
+                    <button className="search-btn" onClick={() => {
+                        console.log(searchText);
+                        let filteredRes = newResList.filter((items) => {
+                            return items.info.name.includes(searchText)
+                        })
+                        console.log(filteredRes);
+                        setNewResList(filteredRes);
+                    }}>Search</button>
+                </div>
+
                 <button className="filter-btn" onClick={() => {
                     const filteredRes = newResList.filter((items) => {
                         return items.info.avgRating > 4
